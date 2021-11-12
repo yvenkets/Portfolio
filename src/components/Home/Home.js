a03b35446fbeb9890a566419a024e5c1e3fd86f0
Venketraman <yvenkets@gmail.com>
2021-11-12 22:31:02 +0530

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -18,8 +18,8 @@ function Home() {
               </h1>
 
               <h1 className="heading-name">
-                I'M
-                <strong className="main-name"> Venket Yuva</strong>
+               This is
+                <strong className="main-name"> Shivam</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
